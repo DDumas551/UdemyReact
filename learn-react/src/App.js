772af1756118f535542435b19c5d9c23820c4095
DDumas551 <david.j.dumas@gmail.com>
2020-11-22 15:04:58 -0500

    streamline

--- learn-react/src/App.js
+++ learn-react/src/App.js
@@ -1,14 +1,15 @@
 import "./App.css";
 import React, { Component } from "react";
-import Pokecard from "./Components/Pokecard/Pokecard";
 import Pokedex from "./Components/Pokedex/Pokedex";
+import "./Components/Pokedex/Pokedex.css"
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        {/* <Pokecard id={4} name="Charmander" type="fire" exp={62} /> */}
-        <Pokedex />
+        <div className="pokedex">
+          <Pokedex />
+        </div>
       </div>
     );
   }
--- learn-react/src/Components/Pokedex/Pokedex.js
+++ learn-react/src/Components/Pokedex/Pokedex.js
@@ -1,9 +1,7 @@
 import React, { Component } from "react";
 import "../Pokedex/Pokedex.css";
+import Pokecard from "../Pokecard/Pokecard";
 const pokemon = require("pokemon");
-
-const Poke_API =
-  "http://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/";
 
 class Pokedex extends Component {
   render() {
@@ -11,14 +9,11 @@ class Pokedex extends Component {
     return (
       <div>
         {allPokemon.map((poke) => (
-          <div>
-            <h1>{poke}</h1>
-            <h1>{pokemon.getId(poke)}</h1>
-            <img
-              className="randompokemon"
-              src={`${Poke_API}${pokemon.getId(poke)}.png`}
-            ></img>
-          </div>
+          <Pokecard
+            name={poke}
+            pokeNumber={pokemon.getId(poke)}
+            id={pokemon.getId(poke)}
+          />
         ))}
       </div>
     );
