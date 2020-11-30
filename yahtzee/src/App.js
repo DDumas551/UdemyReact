1c9c5c60f524c993e7ee4dfa3396ccca835664ca
DDumas551 <david.j.dumas@gmail.com>
2020-11-30 08:21:21 -0500

    Yahtzee starter

--- yahtzee/src/App.js
+++ yahtzee/src/App.js
@@ -1,12 +1,15 @@
-import logo from "./logo.svg";
+import React, { Component } from "react";
+import Game from "./Game";
 import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <Game />
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className='App'>
+        <Game />
+      </div>
+    );
+  }
 }
 
 export default App;
