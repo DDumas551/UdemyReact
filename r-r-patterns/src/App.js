32d4e79993ebaebcfcd6212a404bbca566e848da
DDumas551 <david.j.dumas@gmail.com>
2020-12-02 14:34:25 -0500

    Replaces incorrect files

--- r-r-patterns/src/App.js
+++ r-r-patterns/src/App.js
@@ -1,41 +1,53 @@
 import React, { Component } from "react";
-import Food from "./Food";
-import Meal from "./Meal";
+import Routes from "./Routes";
 import Navbar from "./Navbar";
-import FoodSearch from "./FoodSearch";
-import { Route, Switch } from "react-router-dom";
+import whiskey from "./images/whiskey.jpg";
+import tubby from "./images/tubby.jpg";
+import hazel from "./images/hazel.jpg";
 import "./App.css";
 
 class App extends Component {
+  static defaultProps = {
+    dogs: [
+      {
+        name: "Whiskey",
+        age: 5,
+        src: whiskey,
+        facts: [
+          "Whiskey loves eating popcorn.",
+          "Whiskey is a terrible guard dog.",
+          "Whiskey wants to cuddle with you!"
+        ]
+      },
+      {
+        name: "Hazel",
+        age: 3,
+        src: hazel,
+        facts: [
+          "Hazel has soooo much energy!",
+          "Hazel is highly intelligent.",
+          "Hazel loves people more than dogs."
+        ]
+      },
+      {
+        name: "Tubby",
+        age: 4,
+        src: tubby,
+        facts: [
+          "Tubby is not the brightest dog",
+          "Tubby does not like walks or exercise.",
+          "Tubby loves eating food."
+        ]
+      }
+    ]
+  };
   render() {
     return (
-      <div className='App'>
-        {/* THIS VERSION USING COMPONENT WORKS AND IS SIMPLER */}
-        {/* BUT IT WILL NOT WORK IF YOU NEED TO PASS ADDITIONAL PROPS*/}
-        {/* <Route exact path='/food/:name' scomponent={Food} /> */}
-
-        {/* THIS VERSION USING RENDER IS LESS CLEAN BUT MORE EXPLICIT */}
-        {/* AND YOU CAN PASS IN YOUR OWN ADDITIONAL PROPS */}
-        <Navbar />
-        <Switch>
-          <Route
-            exact
-            path='/food/:name'
-            render={routeProps => <Food {...routeProps} />}
-          />
-          <Route
-            exact
-            path='/food/:foodName/drink/:drinkName'
-            component={Meal}
-          />
-          {/* <Route
-            exact
-            path='/'
-            render={routeProps => <FoodSearch {...routeProps}/>}
-          /> */}
-          <Route exact path='/' component={FoodSearch} />
-          <Route render={() => <h1>ERROR NOT FOUND!!!</h1>} />
-        </Switch>
+      <div>
+        <Navbar dogs={this.props.dogs} />
+        <div className='container'>
+          <Routes dogs={this.props.dogs} />
+        </div>
       </div>
     );
   }
