1c0931d6f386ffb4c78070fe0b5ddfae1f299d69
DDumas551 <david.j.dumas@gmail.com>
2020-12-03 16:11:56 -0500

    Adds Select and Material UI

--- scratch-color-app/src/Palette.js
+++ scratch-color-app/src/Palette.js
@@ -4,26 +4,35 @@ import "./Palette.css";
 import NavBar from "./NavBar";
 import "rc-slider/assets/index.css";
 
-
 export default class Palette extends Component {
   constructor(props) {
     super(props);
-    this.state = { level: 500 };
+    this.state = { level: 500, format: "hex" };
     this.changeLevel = this.changeLevel.bind(this);
+    this.changeFormat = this.changeFormat.bind(this);
   }
   changeLevel(level) {
     this.setState({ level: level });
   }
+  changeFormat(val) {
+    this.setState({
+      format: val,
+    });
+  }
   render() {
-    const { level } = this.state;
+    const { level, format } = this.state;
     const { colors } = this.props.palette;
     const colorBoxes = colors[level].map((color) => (
-      <ColorBox background={color.hex} name={color.name} />
+      <ColorBox background={color[format]} name={color.name} />
     ));
     return (
       <div className="Palette">
         {/* Navbar */}
-        <NavBar level={level} changeLevel={this.changeLevel} />
+        <NavBar
+          level={level}
+          changeLevel={this.changeLevel}
+          handleChange={this.changeFormat}
+        />
         <div className="Palette-colors">
           {/* color boxes */}
           {colorBoxes}
